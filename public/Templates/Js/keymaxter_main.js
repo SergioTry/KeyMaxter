30dcb4600c0908fc701eb550e2e05eebbbc25d17
Sergio <5883001@alu.murciaeduca.es>
2024-02-01 04:52:31 +0100

    Avances en la estructura del proyecto y en el footer

--- public/Templates/Js/keymaxter_main.js
+++ public/Templates/Js/keymaxter_main.js
@@ -1,5 +1,7 @@
 const menuHorizontal = document.getElementById("menuHorizontal");
 const imagenHeader = document.getElementById("imagenHeader");
+const icons = document.getElementsByClassName("rss-icon");
+const footer = document.querySelector("footer");
 
 var menuHorizontalPosicionInicial = menuHorizontal.offsetTop;
 
@@ -9,6 +11,16 @@ window.addEventListener("scroll", function () {
 window.addEventListener("load", function () {
   situarBotonesHorizontales();
 });
+
+for (const icon of icons) {
+  icon.addEventListener("mouseover", function () {
+    footer.style = "backdrop-filter: blur(2.5px)";
+  });
+
+  icon.addEventListener("mouseout", function () {
+    footer.style = "none";
+  });
+}
 
 function esElementoNoVisible(elemento) {
   var rect = elemento.getBoundingClientRect();
@@ -20,9 +32,10 @@ function situarBotonesHorizontales() {
     if (esElementoNoVisible(imagenHeader)) {
       menuHorizontal.style.position = "fixed";
       menuHorizontal.style.top = "0.25rem";
+      menuHorizontal.style.right = "0.5rem";
     } else {
       menuHorizontal.style.position = "absolute";
-      menuHorizontal.style.right = "0.03125rem";
+      menuHorizontal.style.right = "0.5rem";
       menuHorizontal.style.top = menuHorizontalPosicionInicial + "px";
     }
   } else {
