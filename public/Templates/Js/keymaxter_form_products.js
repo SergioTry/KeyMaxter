08264a6d9f7948643c982a17b02eab17b98a7ddb
Sergio <5883001@alu.murciaeduca.es>
2024-02-04 21:21:47 +0100

    Formulario terminado

---
+++ public/Templates/Js/keymaxter_form_products.js
@@ -0,0 +1,42 @@
+const tipoAccion = document.getElementById("tipo-accion");
+
+var textoBoton = document.getElementById("buttonText");
+
+window.addEventListener("load", function () {
+  borrarBanner();
+});
+window.addEventListener("resize", function () {
+  borrarBanner();
+});
+
+tipoAccion.addEventListener("change", function () {
+  textoBoton = document.getElementById("buttonText");
+  if (tipoAccion.value == "1") {
+    textoBoton.textContent = "Modificar";
+  } else {
+    textoBoton.textContent = "Añadir";
+  }
+});
+
+function borrarBanner() {
+  const anchoVentana = document.documentElement.clientWidth;
+  const altoVentana = document.documentElement.clientHeight;
+  const imagenHeader = document.getElementById("header-principal");
+  let formulario = document.querySelector("form");
+  let titulo = document.querySelector("h1");
+  if (anchoVentana <= 950) {
+    imagenHeader.style.display = "none";
+    if (anchoVentana > altoVentana) {
+      titulo.style.fontSize = "0.875rem";
+      formulario.style.overflowY = "scroll";
+      formulario.style.gridTemplateRows = "none";
+      formulario.style.marginBottom = "0.3rem";
+    } else {
+    }
+  } else {
+    imagenHeader.style.display = "flex";
+    titulo.style.fontSize = "1.2rem";
+    formulario.style.overflowY = "hidden";
+    formulario.style.marginBottom = "none";
+  }
+}
